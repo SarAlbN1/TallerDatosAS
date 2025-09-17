9916b7f232ae76914c321c8033342e76ef5b4795
Sara Albarracín <SarAlbN1@users.noreply.github.com>
2025-09-16 21:28:14 -0500

    feat: avance inicial en cliente Spring Boot

---
+++ cliente-productos/cliente-productos/src/test/java/com/ejemplo/cliente/ClienteProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
