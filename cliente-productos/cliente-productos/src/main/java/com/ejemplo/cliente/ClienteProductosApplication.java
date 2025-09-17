a04506c5556a7b16e524cffd5afdf9985bd2b018
Sara Albarracín <SarAlbN1@users.noreply.github.com>
2025-09-16 21:57:21 -0500

    chore: crear estructura en capas y clases vacías para cliente

--- cliente-productos/cliente-productos/src/main/java/com/ejemplo/cliente/ClienteProductosApplication.java
+++ cliente-productos/cliente-productos/src/main/java/com/ejemplo/cliente/ClienteProductosApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClienteProductosApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ClienteProductosApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteProductosApplication.class, args);
+    }
 }
